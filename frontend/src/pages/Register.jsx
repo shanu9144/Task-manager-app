8784a5fdac30b61d7ced7315ca12d4bd1848c203
Shantanu Sharma <118871446+shanu9144@users.noreply.github.com>
2025-01-13 01:08:43 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/register', {
+            const res = await axios.post('https://task-manager-app-k08x.onrender.com/api/auth/register', {
                 username,
                 email,
                 password,
