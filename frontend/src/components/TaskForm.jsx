f89d6df3b74ea494f2d1b148a26eb34c16a9ff4f
Shantanu Sharma <118871446+shanu9144@users.noreply.github.com>
2025-01-13 01:06:57 +0530

    Update TaskForm.jsx

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -43,13 +43,13 @@ const TaskForm = ({ fetchTasks, selectedTask, setSelectedTask }) => {
 
             if (selectedTask) {
                 await axios.put(
-                    `http://localhost:5000/api/tasks/${selectedTask._id}`,
+                    `https://task-manager-app-k08x.onrender.com/api/tasks/${selectedTask._id}`,
                     taskData,
                     config
                 );
                 setSelectedTask(null);
             } else {
-                await axios.post('http://localhost:5000/api/tasks', taskData, config);
+                await axios.post('https://task-manager-app-k08x.onrender.com/api/tasks', taskData, config);
             }
             
             setTitle('');
